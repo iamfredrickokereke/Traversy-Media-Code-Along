f4e6b6c3be984dbc73cace6d7c29e5f687ee3d71
Kelly online <kellyonline2016@gmail.com>
2020-04-01 00:13:26 +0100

    add setHeader method

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const http = require("http");
 
 const app = http.createServer((request, response) => {
 
+    response.statusCode = 200;
+    response.setHeader('Content-type', 'text/plain');
+    response.end("Hello World");
 });
 
 const port = 5500;
@@ -9,4 +12,4 @@ const port = 5500;
 const host = '127.0.0.1';
 
 
-http.listen(port, host, () => { console.log("Server is running on " + port + host)})
+http.listen(port, host, () => { console.log("Server is running on " + port +" hosted on " + host)})
