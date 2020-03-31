b452a3e183372e7ec5b04354dca766e74c0639a2
Kelly online <kellyonline2016@gmail.com>
2020-04-01 00:54:52 +0100

    added asynchronous readfile method

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
 const http = require("http");
 const fs = require("fs");
 
-const app = http.createServer((request, response) => {
-
-    response.statusCode = 200;
-    response.setHeader('Content-type', 'text/plain');
-    response.end("Hello World");
-});
-
 const port = 5500;
 
 const host = '127.0.0.1';
 
 
+fs.readFile("index.html", (error, File ) => {
+
+    if (error) {
+        throw error;
+    }
+
+    const app = http.createServer((request, response) => {
+
+        response.statusCode = 200;
+        response.setHeader('Content-type', 'text/plain');
+        response.write(File)
+        response.end();
+    })
+
+    
+
+
 app.listen(port, host, () => { console.log("Server is running on port " + port +" hosted on " + host)})
+
+
+})
+
+
+
