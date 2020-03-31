851bd72dd43b807d0a45e4af4e3a7775567dd152
Kelly online <kellyonline2016@gmail.com>
2020-04-01 00:09:56 +0100

    add request and response arguments with status codes

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const http = require("http");
 
-const app = http.createServer();
+const app = http.createServer((request, response) => {
+
+});
 
 const port = 5500;
 
-const host = 127.0.0.1;
+const host = '127.0.0.1';
 
 
 http.listen(port, host, () => { console.log("Server is running on " + port + host)})
