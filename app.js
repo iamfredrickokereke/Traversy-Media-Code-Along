fbe9c949dd541a2946d07f1932bad2f3a7c0f3a7
Kelly online <kellyonline2016@gmail.com>
2020-04-01 00:02:12 +0100

    add listening host and port

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = http.createServer();
 
 const port = 5500;
 
-const host = 127. 0 . 0. 1;
+const host = 127.0.0.1;
 
 
-http.listen(port)
+http.listen(port, host, () => { console.log("Server is running on " + port + host)})
