5cb05764218a21af37cee284882df4030a99753e
Kelly online <kellyonline2016@gmail.com>
2020-04-01 10:43:55 +0100

    added a animal class

--- Nodejs-crash-course/person.js
+++ Nodejs-crash-course/person.js
@@ -3,5 +3,16 @@ const person = {
     Age : "30"
 }
 
+class Animal {
+    constructor(name, size){
+        name : this.name;
+        size : this.size
+    }
+
+    greeting(){
+        console.log(`Hey, my name is ${name} and i have a ${size} size.`);
+        
+    }
+}
 
 module.exports = person;
