8c8fa034d8ce0a439c5ddabd15c312d744727c2d
Kelly online <kellyonline2016@gmail.com>
2020-04-02 23:40:22 +0100

    updated

--- Nodejs-crash-course/reference/fs_demo.js
+++ Nodejs-crash-course/reference/fs_demo.js
@@ -6,19 +6,19 @@ console.log(fs);
 
 // create a folder
 
-fs.mkdir(path.join(__dirname, '/test', 'folder119'), (error) => {
-    if (error) {
-        throw error;
-    }
+// fs.mkdir(path.join(__dirname, '/test', 'folder1129'), (error) => {
+//     if (error) {
+//         throw error;
+//     }
 
-    console.log("folder created");
+//     console.log("folder created");
     
-})
+// })
 
 
 // create and write to folder
 
-fs.writeFile('./test/folder119', "hello, it works", (error) => {
+fs.writeFile('./test/folder12219', "hello, it works", (error) => {
 
     if (error) {
         throw error;
