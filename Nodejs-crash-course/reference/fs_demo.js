b61257d42624fcb746d70fa7390d2ad8ba09759e
Kelly online <kellyonline2016@gmail.com>
2020-04-03 00:41:14 +0100

    using the readfile method of fs module

--- Nodejs-crash-course/reference/fs_demo.js
+++ Nodejs-crash-course/reference/fs_demo.js
@@ -18,24 +18,31 @@ console.log(fs);
 
 // create and write to folder
 
-fs.writeFile(path.join(__dirname, './test', 'test.txt' ), "hello, it works", (error) => {
+// fs.writeFile(path.join(__dirname, './test', 'test.txt' ), "hello, it works", (error) => {
 
-    if (error) {
-        throw error;
-    }
+//     if (error) {
+//         throw error;
+//     }
 
-    console.log("file created");
+//     console.log("file created");
 
-    //appending a file by just changing its content and adding to previous
+//     //appending a file by just changing its content and adding to previous
 
-    fs.appendFile(path.join(__dirname, './test', 'test.txt'), ' + 2nd time i was added again', (error) => {
-            if (error) {
-                throw error;
-            }
+//     fs.appendFile(path.join(__dirname, './test', 'test.txt'), ' + 2nd time i was added again', (error) => {
+//             if (error) {
+//                 throw error;
+//             }
         
-            console.log(" new file added");
+//             console.log(" new file added");
             
-        })
+//         })
     
 
+// })
+
+
+// read contents of a file
+
+
+fs.readFile(path.join(__dirname, './test', './test/test.txt'), utf-8, (error, content) => { if (error) throw error; console.log(content);
 })
