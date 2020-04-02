0bc0254824081d8698968d7af949909003471a40
Kelly online <kellyonline2016@gmail.com>
2020-04-02 13:29:38 +0100

    added comments

--- Nodejs-crash-course/app.js
+++ Nodejs-crash-course/app.js
@@ -1,7 +1,10 @@
-
+// using the common js to get the class
 
 const Animal = require('./person')
 
+// using the es6 import method to get class
+
+// import Animal from './person'
 
 const Animal1 = new Animal('monkey', 34)
 
