c964335a93f95c5bd73b24ddd9350d5eb9b02be1
Kelly online <kellyonline2016@gmail.com>
2020-04-03 00:35:54 +0100

    using appended file module

--- Nodejs-crash-course/reference/fs_demo.js
+++ Nodejs-crash-course/reference/fs_demo.js
@@ -18,13 +18,24 @@ console.log(fs);
 
 // create and write to folder
 
-fs.writeFile(path.join(__dirname, './test/folder1129', 'test.txt' ), "hello, it works", (error) => {
+fs.writeFile(path.join(__dirname, './test', 'test.txt' ), "hello, it works", (error) => {
 
     if (error) {
         throw error;
     }
 
     console.log("file created");
+
+    //appending a file by just changing its content and adding to previous
+
+    fs.appendFile(path.join(__dirname, './test', 'test.txt'), ' + 2nd time i was added again', (error) => {
+            if (error) {
+                throw error;
+            }
+        
+            console.log(" new file added");
+            
+        })
     
 
 })
