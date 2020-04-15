a8e5f509d83ee90566cb25d69792b55b5d06de1c
Kelly online <kellyonline2016@gmail.com>
2020-04-15 03:03:05 +0100

    resolved port

--- NEM-Dev-Deployment/app.js
+++ NEM-Dev-Deployment/app.js
@@ -3,7 +3,10 @@ const app = express()
 
 const port = process.env.port || 8080
 
-const host = `http://localhost:{}`
+const host = `http://localhost:${port}`
 app.get()
 
-app.listen()
+app.listen(host, () => { 
+    console.log(`server is running on port ${host}`);
+    
+})
