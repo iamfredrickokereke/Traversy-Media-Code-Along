ba5023c986df5982204cf3b0cd14582ae887cf9c
Kelly Online <Kellyonline2016@gmail.com>
2020-04-15 04:00:32 +0100

    fix host path

--- NEM-Dev-Deployment/app.js
+++ NEM-Dev-Deployment/app.js
@@ -3,10 +3,9 @@ const app = express()
 
 const port = process.env.port || 8080
 
-const host = `http://localhost:${port}`
 app.get()
 
 app.listen(host, () => { 
-    console.log(`server is running on port ${host}`);
+    console.log(`server is running on port http://localhost:${host}`);
     
 })
