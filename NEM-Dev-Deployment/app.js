a9f730e826de935fb6a82750031a9e074c22ca87
Kelly online <kellyonline2016@gmail.com>
2020-04-18 12:56:46 +0100

    updated with remote

--- NEM-Dev-Deployment/app.js
+++ NEM-Dev-Deployment/app.js
@@ -1,25 +1,8 @@
-<<<<<<< HEAD
-const express = require('express');
-const app = express();
-=======
 const express = require('express')
 const app = express()
 const exphbs = require('express-handlebars')
->>>>>>> 552f6fa62266aeac4c8b0e924d4c72f1047ec694
 
 
-<<<<<<< HEAD
-// const port = 8080;
-
-const host = 'http://localhost:';
-
-app.get('/', (req, res) => {
-    res.send("it works");
-})
-
-app.listen(host, () => { 
-    console.log(`server is running on port `+ host + port);
-=======
 const port = process.env.PORT ||8000
 
 app.engine('handlebars', exphbs({defaultLayout : "main"}))
@@ -46,11 +29,10 @@ app.get('/about', (req, res) => {
     //req.name ="oh boy!"
     // console.log(req.name);
     
-    res.send("ABOUT")
+    res.render("ABOUT")
 })
 
 app.listen(port, () => { 
     console.log(`server is running on port http://localhost:${port}`);
->>>>>>> 552f6fa62266aeac4c8b0e924d4c72f1047ec694
     
 })
